e8e5951339a58587115fc0ee270a57bb8ea83e07
Iván Gilabert Sabio <Ivan.Gilabert.Sabio@unit4.com>
2020-11-16 11:40:34 +0100

    Added logic to run the program

---
+++ MartianRobots/MartianRobots/Configuration/AppConfigurationBuilder.cs
@@ -0,0 +1,29 @@
+﻿using MartianRobots.CommandHandlers;
+using MartianRobots.Control;
+using MartianRobots.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace MartianRobots.Configuration
+{
+    public class AppConfigurationBuilder
+    {
+        private readonly IServiceCollection _services;
+
+        public AppConfigurationBuilder()
+        {
+            _services = new ServiceCollection();
+        }
+
+        public IServiceProvider Build()
+        {
+            _services.AddSingleton<IControlCenter, ControlCenter>();
+            _services.AddSingleton<IRobotsContext, RobotsContext>();
+            _services.AddSingleton<ICommandHandler, SetSurfaceCommandHandler>();
+            _services.AddSingleton<ICommandHandler, SetStartPossitionCommandHandler>();
+            _services.AddSingleton<ICommandHandler, MoveRobotCommandHandler>();
+
+            return _services.BuildServiceProvider();
+        }
+    }
+}
--- MartianRobots/MartianRobots/Program.cs
+++ MartianRobots/MartianRobots/Program.cs
@@ -1,11 +1,43 @@
-﻿using System;
+﻿using Dawn;
+using MartianRobots.Configuration;
+using MartianRobots.Control;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
 
 namespace MartianRobots
 {
     class Program
     {
+        private static IServiceProvider _serviceProvider;
+        private static IControlCenter _controlCenter;
+
         static void Main(string[] args)
         {
+            var commands = LoadCommands(args);
+
+            ConfigureApp();
+            InitializeControlCenter();
+
+            foreach (var command in commands)
+            {
+                _controlCenter.SendCommand(command.ToUpper());
+            }
+
+            Console.Write(_controlCenter.GetResulsts());
         }
+
+        private static string[] LoadCommands(string[] args)
+        {
+            Guard.Argument(args).NotNull().Count(1);
+            return File.ReadAllLines(args[0]);
+        }
+
+        private static void ConfigureApp()
+        {
+            _serviceProvider = new AppConfigurationBuilder().Build();
+        }
+
+        private static void InitializeControlCenter() => _controlCenter = _serviceProvider.GetService<IControlCenter>();
     }
 }
